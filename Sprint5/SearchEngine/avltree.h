117688ac072b79d537a04e293fea8b967fc43d25
PatrickDeVries <pdevries@smu.edu>
2018-11-09 22:44:30 -0600

    added AVL rotations

--- Sprint5/SearchEngine/avltree.h
+++ Sprint5/SearchEngine/avltree.h
@@ -37,7 +37,16 @@ public:
     AVLTree(const AVLTree<fontenot>& rhs);
     AVLTree operator=(const AVLTree<fontenot>& val);
     ~AVLTree();
+    void insert(const fontenot& val);
+
+private:
     void insert(const fontenot& val, TreeNode<fontenot> *& t);
+    void rotateWithLeftChild(TreeNode<fontenot>*& k2);
+    void doubleWithLeftChild(TreeNode<fontenot>*& k3);
+    void rotateWithRightChild(TreeNode<fontenot>*& k2);
+    void doubleWithRightChild(TreeNode<fontenot>*& k3);
+    int max(int l, int r);
+
 };
 
 template<typename fontenot>
@@ -51,6 +60,12 @@ template<typename fontenot>
 AVLTree<fontenot>::~AVLTree()
 {
 
+}
+
+template<typename fontenot>
+void AVLTree<fontenot>::insert(const fontenot &val)
+{
+    insert(val, root);
 }
 
 template<typename fontenot>
@@ -87,6 +102,50 @@ void AVLTree<fontenot>::insert(const fontenot& val, TreeNode<fontenot> *& t)
         t->data = t->data + val;
     }
 
+    t->height = max(t->left->height, t->right->height) + 1;
+}
 
+template <typename fontenot>
+void AVLTree<fontenot>::rotateWithLeftChild(TreeNode<fontenot> *& k2)
+{
+    TreeNode<fontenot> k1 = k2->left;
+    k2->left = k1->right;
+    k1->right = k2;
+    k2->height = max(k2->left->height, k2->right->height) + 1;
+    k1->height = max(k1->left->height, k2->height) + 1;
+    k2 = k1;
+}
+
+template <typename fontenot>
+void AVLTree<fontenot>::doubleWithLeftChild(TreeNode<fontenot> *&k3)
+{
+    rotateWithRightChild(k3->right);
+    rotateWithLeftChild(k3);
+}
+
+template <typename fontenot>
+void AVLTree<fontenot>::rotateWithRightChild(TreeNode<fontenot> *& k2)
+{
+    TreeNode<fontenot> k1 = k2->right;
+    k2->right = k1->left;
+
+}
+
+template <typename fontenot>
+void AVLTree<fontenot>::doubleWithRightChild(TreeNode<fontenot> *&k3)
+{
+    rotateWithLeftChild(k3->left);
+    rotateWithRightChild(k3);
+}
+
+template <typename fontenot>
+int AVLTree<fontenot>::max(int l, int r)
+{
+    {
+        if (l > r)
+            return l;
+        else
+            return r;
+    }
 }
 #endif // AVLTREE_H
--- Sprint5/SearchEngine/main.cpp
+++ Sprint5/SearchEngine/main.cpp
@@ -7,5 +7,4 @@ int main(int argc, char* argv[])
 {
     cout << "Hello World!" << endl;
     return 0;
-
 }
