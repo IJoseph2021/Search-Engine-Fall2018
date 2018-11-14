61f03bbd258a73fcbea098a978bd9d7330bcb23d
PatrickDeVries <pdevries@smu.edu>
2018-11-14 17:40:53 -0600

    comments on AVLtree

--- Sprint5/SearchEngine/avltree.h
+++ Sprint5/SearchEngine/avltree.h
@@ -136,19 +136,24 @@ AVLTree<fontenot>& AVLTree<fontenot>::operator =(const AVLTree<fontenot>& val)
 template<typename fontenot>
 AVLTree<fontenot>::~AVLTree()
 {
+    //only delete root because nodes delete all descendants
     delete root;
     root = nullptr;
+    nodes = 0;
 }
 
+//print items of a tree in orer
 template<typename fontenot>
 void AVLTree<fontenot>::printInOrder(ostream& out)
 {
     if (root != nullptr)
         printInOrder(out, root);
     else
-        out << "Empty tree" << endl;
+        throw logic_error("Empty tree in AVLtree:printInOrder");
 }
 
+//public insert which calls private recursive insert algorithm
+//then increments node counter
 template<typename fontenot>
 void AVLTree<fontenot>::insert(const fontenot &val)
 {
@@ -164,6 +169,8 @@ void AVLTree<fontenot>::clear()
     nodes = 0;
 }
 
+//print items of tree in order by recursively calling with left trees
+//then printing, then recursively calling with right trees
 template <typename fontenot>
 void AVLTree<fontenot>::printInOrder(ostream &out, TreeNode<fontenot>* t)
 {
@@ -179,17 +186,24 @@ void AVLTree<fontenot>::printInOrder(ostream &out, TreeNode<fontenot>* t)
     }
 }
 
-
+//find where passed value should go by desending the tree via
+//recursive calls in the appropriate direction, then checking balance
+//and rebalancing as the recursion stack is popped
 template<typename fontenot>
 void AVLTree<fontenot>::insert(const fontenot& val, TreeNode<fontenot> *& t)
 {
+    //if no value at current node, create node at that point
     if (t == nullptr)
     {
         t = new TreeNode<fontenot>(val);
     }
+    //if the passed value is less than the value of the current node's data
     else if(val < t->data)
     {
+        //descend tree left
         insert(val, t->left);
+
+        //check if balanced at node, if not then balance
         if (height(t->left) - height(t->right)== 2)
         {
             if (val < t->left->data)
@@ -198,9 +212,13 @@ void AVLTree<fontenot>::insert(const fontenot& val, TreeNode<fontenot> *& t)
                 doubleWithLeftChild(t);
         }
     }
+    //if passed value is greater than the value of the current node's data
     else if (val > t->data)
     {
+        //descend right
         insert(val, t->right);
+
+        //check if balanced, if not then balance
         if (height(t->right) - height(t->left) == 2)
         {
             if (val > t->right->data)
@@ -209,25 +227,40 @@ void AVLTree<fontenot>::insert(const fontenot& val, TreeNode<fontenot> *& t)
                 doubleWithRightChild(t);
         }
     }
+    //if the item already exists in the tree combine it into the existing item
+    //this is implemented for the sake of the word class to track duplicate words
+    //number of appearances in each document
     else
     {
         t->data = t->data + val;
     }
 
+    //find new height of node
     t->height = max(height(t->left), height(t->right)) + 1;
 }
 
+//rotate with left child balances a tree around an alpha k2 that
+//is unbalanced in a case 1 scecnario
 template <typename fontenot>
 void AVLTree<fontenot>::rotateWithLeftChild(TreeNode<fontenot> *& k2)
 {
+    //create k1 node to track left item of k2
     TreeNode<fontenot>* k1 = k2->left;
+
+    //swap pointers appropriately
     k2->left = k1->right;
     k1->right = k2;
+
+    //readjust heights
     k2->height = max(height(k2->left), height(k2->right)) + 1;
     k1->height = max(height(k1->left), height(k2)) + 1;
+
+    //replace k2 with final k1 node
     k2 = k1;
 }
 
+//double with left child solves a case 2 at alpha k3 by first rotating its right
+//tree with its right child, creating a case 1, then rotating alpha with its left child
 template <typename fontenot>
 void AVLTree<fontenot>::doubleWithLeftChild(TreeNode<fontenot> *&k3)
 {
@@ -235,6 +268,7 @@ void AVLTree<fontenot>::doubleWithLeftChild(TreeNode<fontenot> *&k3)
     rotateWithLeftChild(k3);
 }
 
+//rotate with right child solves case 4 by rotating alpha k2 with its right child
 template <typename fontenot>
 void AVLTree<fontenot>::rotateWithRightChild(TreeNode<fontenot> *& k2)
 {
@@ -246,6 +280,8 @@ void AVLTree<fontenot>::rotateWithRightChild(TreeNode<fontenot> *& k2)
     k2 = k1;
 }
 
+//double with right child solves case 3 by rotating alpha k3's left child with its left child
+//creating a case 4, then solving that by rotating k3 with its right child
 template <typename fontenot>
 void AVLTree<fontenot>::doubleWithRightChild(TreeNode<fontenot> *&k3)
 {
@@ -253,6 +289,7 @@ void AVLTree<fontenot>::doubleWithRightChild(TreeNode<fontenot> *&k3)
     rotateWithRightChild(k3);
 }
 
+//find which of two values is the largest
 template <typename fontenot>
 int AVLTree<fontenot>::max(int l, int r)
 {
@@ -264,6 +301,7 @@ int AVLTree<fontenot>::max(int l, int r)
     }
 }
 
+//get the height with precaution for uninitialized nodes
 template <typename fontenot>
 int AVLTree<fontenot>::height(TreeNode<fontenot> *t)
 {
@@ -273,6 +311,7 @@ int AVLTree<fontenot>::height(TreeNode<fontenot> *t)
         return t->height;
 }
 
+//find smallest item in the tree by descending all the way left
 template <typename fontenot>
 fontenot& AVLTree<fontenot>::findMin()
 {
@@ -284,6 +323,7 @@ fontenot& AVLTree<fontenot>::findMin()
     return temp->data;
 }
 
+//find largest item in the tree by descending all the way right
 template <typename fontenot>
 fontenot& AVLTree<fontenot>::findMax()
 {
@@ -295,6 +335,7 @@ fontenot& AVLTree<fontenot>::findMax()
     return temp->data;
 }
 
+//public contains function to call private contains function if tree exists
 template <typename fontenot>
 bool AVLTree<fontenot>::contains(fontenot val)
 {
@@ -304,22 +345,29 @@ bool AVLTree<fontenot>::contains(fontenot val)
         return contains(root, val);
 }
 
+//private recursive contains which uses binary search to search the avl
+//tree for an item and return true if it exists
 template <typename fontenot>
 bool AVLTree<fontenot>::contains(TreeNode<fontenot> *& t, fontenot val)
 {
+    //return that it exists if this node data is the value
     if (t->data == val)
         return true;
+    //if value is smaller than node data descend left
     else if (t->data > val)
     {
         if (t->left != nullptr)
             return contains(t->left, val);
+        //if the value is smaller than node data but there is no left, return false
         else
             return false;
     }
+    //if value is greater than node data descent right
     else if (t->data < val)
     {
         if (t->right != nullptr)
             return contains(t->right, val);
+        //if value is larger than node data but there is no rigt, return false
         else
             return false;
     }
@@ -329,6 +377,7 @@ bool AVLTree<fontenot>::contains(TreeNode<fontenot> *& t, fontenot val)
     }
 }
 
+//public remove function to recursively call remove funciton
 template <typename fontenot>
 void AVLTree<fontenot>::remove(fontenot val)
 {
@@ -337,65 +386,77 @@ void AVLTree<fontenot>::remove(fontenot val)
     remove(toRemove);
 }
 
+//private remove function which can delete leaves
 template <typename fontenot>
 void AVLTree<fontenot>::remove(TreeNode<fontenot>*& toRemove)
 {
+    //if node is a leaf simply delete
     if (toRemove->height == 0)
     {
         delete toRemove;
         return;
     }
-    else if (toRemove->height == 1 && (toRemove->left == nullptr || toRemove->right == nullptr))
+    else
     {
-        if (toRemove->left == nullptr)
-            toRemove = toRemove->right;
-        else
-            toRemove = toRemove->left;
-        return;
+        throw logic_error("Not a leaf in AVLTree:remove()");
     }
 }
 
+//public find function to call recursive binary search on root and return the foudn item
 template <typename fontenot>
 fontenot& AVLTree<fontenot>::find(fontenot val)
 {
     return find(val,root).data;
 }
 
+//private search function which binary searches for an item by stepping through the tree using comparisons
+//and returning the item it ends on
 template <typename fontenot>
 AVLTree<fontenot>::TreeNode<fontenot>& AVLTree<fontenot>::find(fontenot& val, TreeNode<fontenot>* t)
 {
+    //if this is the target value return the node
     if (t->data == val)
         return *t;
-    if (t->data > val)
+    //if the value is less than the current node go left
+    else if (t->data > val)
     {
         if (t->left != nullptr)
             return find(val, t->left);
-        else
+        else            
+            //if the value is less than the current node but there is no left node throw error
             throw logic_error("Value not in tree [in find()]");
     }
-    if (t->data < val)
+    //if the value is greater than the current node go right
+    else if (t->data < val)
     {
         if (t->right != nullptr)
             return find(val, t->right);
         else
+            //if the value is greater but there is no right node throw error
             throw logic_error("Value not in tree [in find()]");
     }
 }
 
+//copy nodes used for operator= and copy constructor to
+//recursively create deep copies of a passed tree's nodes
 template <typename fontenot>
 AVLTree<fontenot>::TreeNode<fontenot>* AVLTree<fontenot>::copyNodes(TreeNode<fontenot> *t)
 {
+    //if the node exists
     if (t!= nullptr)
     {
+        //create copy of left
         TreeNode<fontenot>* left = copyNodes(t->left);
+        //create copy of right
         TreeNode<fontenot>* right = copyNodes(t->right);
+        //return the values as a new node
         return new TreeNode<fontenot>(t->data, left, right);
     }
+    //if node does not exists return a nullptr
     else
     {
         return nullptr;
     }
-
 }
 
 #endif // AVLTREE_H
