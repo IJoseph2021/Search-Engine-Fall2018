5716d780b6f227e8aa2b82203529167ea5ddd1e9
Isaac Joseph <ijoseph@smu.edu>
2018-11-27 03:50:34 -0600

    Added HashNode

--- Sprint5/SearchEngine/hashtable.h
+++ Sprint5/SearchEngine/hashtable.h
@@ -1,6 +1,37 @@
+//owner: Isaac Joseph
+//created: 11/26/2018
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+#include <vector>
+#include <string>
+#include <functional>
 
+using namespace std;
+
+template<typename mark, typename fontenot>
+
+class HashNode{
+private:
+    mark key;
+    fontenot value;
+public:
+    HashNode(mark a, fontenot b){
+        key = a;
+        value = b;
+    }
+
+    HashNode(const HashNode<mark, fontenot>& a){
+        this->key = a.key;
+        this->value = a.value;
+    }
+    HashNode& operator =(const HashNode<mark, fontenot>& a){
+        this->key = a.key;
+        this->value = a.value;
+    }
+    ~HashNode(){
+
+    }
+};
 
 #endif // HASHTABLE_H
