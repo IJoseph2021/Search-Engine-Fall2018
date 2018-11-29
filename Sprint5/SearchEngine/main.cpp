e65a3ffdb0896d5693d17aab41f0afe10f1072d0
Nathan Srirama <nsrirama@smu.edu>
2018-11-29 00:50:58 -0600

    Fixed error where user mode couldn't exit

--- Sprint5/SearchEngine/main.cpp
+++ Sprint5/SearchEngine/main.cpp
@@ -204,6 +204,10 @@ void userMode(IndexerFace* avS, IndexerFace* haS, IndexerFace* avD, IndexerFace*
             cout << "Average Number of Words Indexed per Opinion: " << avg << endl;
             break;
         } //case 3
+        case 4:{
+            userFlag = false;
+            break;
+        }//case 4
         default:
             cout << "You entered an invalid number \n\n" << endl;
         } //end switch
